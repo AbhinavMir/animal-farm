33bcc71e1e4c29f508b509b269e2daf5f4c077c6
Abhinav Srivastava <atg271@gmail.com>
2022-10-17 19:47:50 -0400

    Simple Example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("Bradley", 5, "Hey");
+        System.out.println(animal.getName());
+        System.out.println(animal.getAge());
+        System.out.println(animal.getWeight());
+    }
+}
+
+class Animal <T> {
+    private String name;
+    private int age;
+    private T weight; // generic type - to account for floats, doubles, etc.
+
+    public Animal(String name, int age, T weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public T getWeight() {
+        return weight;
     }
 }
