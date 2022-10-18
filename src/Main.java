618ecd5adc6ac717f156c0c299fa83792a1fa6b1
Abhinav Srivastava <atg271@gmail.com>
2022-10-18 00:40:55 -0400

    wip

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,67 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] weight = {1, 2, 3, 4, 5};
-        int[] height = {5, 4, 3, 2, 1};
+        int[] age = {5, 4, 3, 2, 1};
         String[] name = {"a", "b", "c", "d", "e"};
         ArrayList<Animal> animals = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            animals.add(new Animal(weight[i], height[i], name[i]));
+            animals.add(new Animal(weight[i], age[i], name[i]));
         }
 
-        Collections.sort(animals);
+        Comparator<Animal> comparator1 = new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                if (o1.weight > o2.weight) {
+                    return 1;
+                } else if (o1.weight < o2.weight) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+
+        Comparator<Animal> comparator2 = new Comparator<Animal>() {
+            @Override
+            public int compare(Animal o1, Animal o2) {
+                if (o1.age > o2.age) {
+                    return 1;
+                } else if (o1.age < o2.age) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1 to sort by weight, 2 to sort by age");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            Collections.sort(animals, comparator1);
+        } else if (choice == 2) {
+            Collections.sort(animals, comparator2);
+        } else {
+            System.out.println("Wrong choice");
+        }
+
         for (Animal animal : animals) {
             System.out.println(animal.weight + " " + animal.age + " " + animal.name);
         }
     }
 }
 
-class Animal implements Comparable<Animal> {
+class Animal {
     int age;
     String name;
     int weight;
 
-    public Animal(int weight, int height, String name) {
+    public Animal(int weight, int age, String name) {
         this.weight = weight;
-        this.age = height;
+        this.age = age;
         this.name = name;
-    }
-
-    @Override
-    public int compareTo(Animal o) {
-        if (o.age > this.age) return -1;
-        else if (o.age < this.age) return 1;
-        else return 0;
     }
 }
