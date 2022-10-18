40228057bc86cbdc86e643773803dc98af372536
Abhinav Srivastava <atg271@gmail.com>
2022-10-17 20:05:52 -0400

    wip

--- src/Dog.java
+++
@@ -1,9 +0,0 @@
-public class Dog extends Animal {
-    public Dog(String name, int age, int weight) {
-        super(name, age, weight);
-    }
-
-    public String bark() {
-        return "Woof!";
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Try entering a number on name and a string on age and see what happens!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter animal name: ");
+        String name = scanner.next();
+        System.out.println("Enter animal weight: (try out all types)");
+        int weight = scanner.nextInt();
+
         try {
-            Dog dog = new Dog("Brutus", 5, 15);
-            System.out.println(dog.bark());
-            System.out.println(dog.getName());
-            System.out.println(dog.getAge());
-            System.out.println(dog.getWeight());
+            Animal animal = new Animal(name, 12, weight);
+
+            System.out.println(animal.getName());
+            System.out.println(animal.getAge());
+            System.out.println(animal.getWeight());
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 }
 
-class Animal {
-    private String name;
-    private int age;
-    private int weight;
+class Animal<T extends Number, V extends String> {
+    private final V name;
+    private final int age;
+    private final T weight; // generic type - to account for floats, doubles, etc.
 
-    public Animal(String name, int age, int weight) {
+    public Animal(V name, int age, T weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
@@ -33,7 +40,7 @@ class Animal {
         return age;
     }
 
-    public int getWeight() {
+    public T getWeight() {
         return weight;
     }
 }
